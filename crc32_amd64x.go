d7104f5ae105f017bc05710e9ecba7384e625e05
Klaus Post <klauspost@gmail.com>
2015-08-20 12:21:59 +0200

    Check for combined CLMUL and SSE 4.1 at startup.

--- crc32_amd64x.go
+++ crc32_amd64x.go
@@ -7,7 +7,7 @@
 package crc32
 
 // This file contains the code to call the SSE 4.2 version of the Castagnoli
-// CRC.
+// and IEEE CRC.
 
 // haveSSE41/haveSSE42 is defined in crc_amd64.s and uses CPUID to test
 // for SSE 4.1 and 4.2 support.
@@ -20,12 +20,11 @@ func haveCLMUL() bool
 func castagnoliSSE42(crc uint32, p []byte) uint32
 
 // ieeeCLMUL is defined in crc_amd64.s and uses the PCLMULQDQ
-// instruction.
+// instruction as well as SSE 4.1.
 func ieeeCLMUL(crc uint32, p []byte) uint32
 
-var sse41 = haveSSE41()
 var sse42 = haveSSE42()
-var clmul = haveCLMUL()
+var clmulSse41 = haveCLMUL() && haveSSE41()
 
 func updateCastagnoli(crc uint32, p []byte) uint32 {
 	if sse42 {
@@ -35,7 +34,7 @@ func updateCastagnoli(crc uint32, p []byte) uint32 {
 }
 
 func updateIEEE(crc uint32, p []byte) uint32 {
-	if clmul && sse41 && len(p) >= 64 {
+	if clmulSse41 && len(p) >= 64 {
 		left := len(p) & 15
 		do := len(p) - left
 		crc := ^ieeeCLMUL(^crc, p[:do])
