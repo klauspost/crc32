a5d1ea1c7bb4ad05a27df06491071dd6d45d1a08
Klaus Post <klauspost@gmail.com>
2015-09-01 09:06:28 +0200

    Sync with proposed standard library.

--- crc32_amd64p32.go
+++ crc32_amd64p32.go
@@ -2,29 +2,20 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build amd64 amd64p32
-
 package crc32
 
 // This file contains the code to call the SSE 4.2 version of the Castagnoli
-// and IEEE CRC.
+// CRC.
 
-// haveSSE41/haveSSE42 is defined in crc_amd64.s and uses CPUID to test
-// for SSE 4.1 and 4.2 support.
-func haveSSE41() bool
+// haveSSE42 is defined in crc_amd64p32.s and uses CPUID to test for 4.2
+// support.
 func haveSSE42() bool
-func haveCLMUL() bool
 
 // castagnoliSSE42 is defined in crc_amd64.s and uses the SSE4.2 CRC32
 // instruction.
 func castagnoliSSE42(crc uint32, p []byte) uint32
 
-// ieeeCLMUL is defined in crc_amd64.s and uses the PCLMULQDQ
-// instruction as well as SSE 4.1.
-func ieeeCLMUL(crc uint32, p []byte) uint32
-
 var sse42 = haveSSE42()
-var clmulSse41 = haveCLMUL() && haveSSE41()
 
 func updateCastagnoli(crc uint32, p []byte) uint32 {
 	if sse42 {
@@ -34,14 +25,13 @@ func updateCastagnoli(crc uint32, p []byte) uint32 {
 }
 
 func updateIEEE(crc uint32, p []byte) uint32 {
-	if clmulSse41 && len(p) >= 64 {
-		left := len(p) & 15
-		do := len(p) - left
-		crc := ^ieeeCLMUL(^crc, p[:do])
-		if left > 0 {
-			crc = update(crc, IEEETable, p[do:])
-		}
-		return crc
+	// only use slicing-by-8 when input is >= 4KB
+	if len(p) >= 4096 {
+		iEEETable8Once.Do(func() {
+			iEEETable8 = makeTable8(IEEE)
+		})
+		return updateSlicingBy8(crc, iEEETable8, p)
 	}
+
 	return update(crc, IEEETable, p)
 }
--- crc32_generic.go
+++ crc32_generic.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build 386 arm
+// +build 386 arm arm64 ppc64 ppc64le
 
 package crc32
 
@@ -14,5 +14,12 @@ func updateCastagnoli(crc uint32, p []byte) uint32 {
 }
 
 func updateIEEE(crc uint32, p []byte) uint32 {
+	// only use slicing-by-8 when input is >= 4KB
+	if len(p) >= 4096 {
+		iEEETable8Once.Do(func() {
+			iEEETable8 = makeTable8(IEEE)
+		})
+		return updateSlicingBy8(crc, iEEETable8, p)
+	}
 	return update(crc, IEEETable, p)
 }
