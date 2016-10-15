910881aa3be589da2ed5535c99fd4d0ff8fc526a
Klaus Post <klauspost@gmail.com>
2016-10-15 19:26:47 +0200

    Remove build restrictions

--- crc32_generic.go
+++ crc32_generic.go
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// +build !amd64,!amd64p32 appengine gccgo
-
 // This file contains CRC32 algorithms that are not specific to any architecture
 // and don't use hardware acceleration.
 //
